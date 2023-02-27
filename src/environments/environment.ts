bd61409a0450781290b3da5969b08fe8aea2b377
Chukwuemeka Eze <c.eze@outlook.com>
2023-02-27 17:44:16 +0100

    frontend that works locally with the api4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,21 +2,41 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false,
-  apiUrl: process.env.apiUrl,
-  loginUrl: process.env.loginUrl,
-  loginUrl2: process.env.loginUrl2,
+  production: true,
+  apiUrl: 'http://localhost:3000',
+  loginUrl: 'http://localhost:7474',
+  loginUrl2: 'http://localhost:7474',
   // loginUrl2: 'http://192.168.92.222:7474',
-  dropboxKEY: process.env.dropboxKEY,
-  dropboxUrl: process.env.dropboxUrl,
+  dropboxKEY: 'pd8hvwf9oc0zpfg',
+  dropboxUrl: 'http://localhost:4200/nav/studentinfo',
   // neo4jAPI: 'http://localhost:3000',
-  neo4jAPI: process.env.neo4jAPI,
+  neo4jAPI: 'https://kpay-api.onrender.com',
+
   dropboxToken: '' ,
   send2Reg: false,
   authSuccess: false
 
 };
+
+
+
+// export const environment = {
+//   production: false,
+//   apiUrl: process.env.apiUrl,
+//   loginUrl: process.env.loginUrl,
+//   loginUrl2: process.env.loginUrl2,
+//   // loginUrl2: 'http://192.168.92.222:7474',
+//   dropboxKEY: process.env.dropboxKEY,
+//   dropboxUrl: process.env.dropboxUrl,
+//   // neo4jAPI: 'http://localhost:3000',
+//   neo4jAPI: process.env.neo4jAPI,
+//   dropboxToken: '' ,
+//   send2Reg: false,
+//   authSuccess: false
+//
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
